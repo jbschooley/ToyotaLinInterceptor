fcac2ab468c33789465a6e9b0f9c9edc115e09d0
Jacob Schooley <jacob@schooley.com>
2022-12-27 09:55:20 -0700

    add CarHandler with nothing in it

---
+++ src/CarHandler.h
@@ -0,0 +1,13 @@
+//
+// Created by Jacob on 12/25/2022.
+//
+
+#ifndef TOYOTALININTERCEPTOR_CARHANDLER_H
+#define TOYOTALININTERCEPTOR_CARHANDLER_H
+#include <Arduino.h>
+
+class CarHandler {
+
+};
+
+#endif //TOYOTALININTERCEPTOR_CARHANDLER_H
--- src/PanelHandler.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Jacob on 11/21/2022.
-//
-
-#include "PanelHandler.h"
