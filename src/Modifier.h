372a34c3734aab0fb83988e2c0fd2d22161b873c
Jacob Schooley <jacob@schooley.com>
2023-01-29 16:33:42 -0700

    streamline modifier functions

--- src/Modifier.h
+++ src/Modifier.h
@@ -12,7 +12,7 @@ private:
     Logger* l;
     DataStore* ds;
 
-public:
+    // buttons {byte, mask}
     const uint8_t BUTTON_OFF[2] =           {0, 0x02};
     const uint8_t BUTTON_AUTO[2] =          {0, 0x08};
     const uint8_t BUTTON_SYNC[2] =          {3, 0x20};
@@ -24,20 +24,24 @@ public:
     const uint8_t BUTTON_FAN_UP[2] =        {1, 0x3c};
     const uint8_t BUTTON_MODE[2] =          {2, 0x1c};
     const uint8_t BUTTON_RECYCLE[2] =       {6, 0xc0};
-    const uint8_t BUTTON_S_MODE[2] =        {0, 0x80};
+    const uint8_t BUTTON_S_MODE[2] =        {0, 0x80}; // TODO does not seem to be correct, need to monitor button again
+    const uint8_t ZONE_DRIVER[2] =          {4, 0x01};
+    const uint8_t ZONE_PASSENGER[2] =       {5, 0x01};
 
-    const uint8_t TEMP_DRIVER[2] =          {4, 0x01};
-    const uint8_t TEMP_PASSENGER[2] =       {5, 0x01};
+    // car status
+    bool statusFrontDefrost() { return bool(ds->xB1[2] & 0x08); }
+    bool statusRearDefrost() { return bool(ds->xB1[3] & 0x40); }
+    bool statusSync() { return bool(ds->xB1[3] & 0x20); }
+    bool statusEco() { return bool(ds->xB1[0] & 0x08); }
+    bool statusSMode() { return bool(ds->xB1[0] & 0x80); }
+    int statusFanSpeed() { return ds->xB1[1] & 7; }
+    int statusTemp(const uint8_t* zone) { return calcTemp(ds->xB1[zone[0]]); }
 
+    // max and min temps
     enum Temp {
         TEMP_LO = 59,
-        TEMP_HI = 86,
+        TEMP_HI = 86
     };
-
-    explicit Modifier(DataStore* ds) {
-        this->l = new Logger("Modifier", true);
-        this->ds = ds;
-    }
 
     void setButtonsModified() {
         if (!ds->buttonsModifiedSinceLastSend) {
@@ -56,10 +60,60 @@ public:
         ds->x39Mod[zone[0]] += zone[1] * delta;
     }
 
+    bool setTemp(const uint8_t* zone, const uint8_t newTemp) {
+        int delta = getTempDelta(statusTemp(zone), newTemp);
+        if (delta != 0) {
+            setButtonsModified();
+            ds->x39Mod[zone[0]] += zone[1] * delta;
+            return true;
+        }
+        return false;
+    }
+
+    bool setFan(const uint8_t newFanSpeed) {
+        int delta = getFanDelta(statusFanSpeed(), newFanSpeed);
+        if (delta != 0) {
+            setButtonsModified();
+            delta > 0 ? pressButton(BUTTON_FAN_UP) : pressButton(BUTTON_FAN_DOWN);
+            return true;
+        }
+        return false;
+    }
+
     void changeTempLog(const uint8_t* zone, uint8_t delta) {
         l->log("before: " + DataStore::frameToString(ds->x39));
         changeTemp(zone, delta);
         l->log("after:  " + DataStore::frameToString(ds->x39));
+    }
+
+    static uint8_t calcTemp(uint8_t hexTemp) {
+        if (hexTemp == 0) {
+            return TEMP_LO;
+        } else if (hexTemp >= 0x66) {
+            return hexTemp - 0x2a;
+        } else {
+            return hexTemp + 0x1f;
+        }
+    }
+
+    static int getTempDelta(const int currTemp, const int newTemp) {
+        return bounds(newTemp - currTemp, -15, 15);
+    }
+
+    static int getFanDelta(const int currFan, const int newFan) {
+        return bounds(newFan - currFan, -1, 1);
+    }
+
+    static int bounds(int val, int min, int max) {
+        if (val < min) val = min;
+        else if (val > max) val = max;
+        return val;
+    }
+
+public:
+    explicit Modifier(DataStore* ds) {
+        this->l = new Logger("Modifier", true);
+        this->ds = ds;
     }
 
     // TEST STUFF
@@ -99,64 +153,31 @@ public:
 
             if (!oneTimeSettingsSet) {
                 // defrost
-                if (!bool(ds->xB1[2] & 0x08)) {
-                    pressButton(BUTTON_FRONT_DEFROST);
-                }
-                if (!bool(ds->xB1[3] & 0x40)) {
-                    pressButton(BUTTON_REAR_DEFROST);
-                }
+                if (!statusFrontDefrost()) { pressButton(BUTTON_FRONT_DEFROST); }
+                if (!statusRearDefrost()) { pressButton(BUTTON_REAR_DEFROST); }
 
                 // sync on
-                if (!bool(ds->xB1[3] & 0x20)) {
-                    pressButton(BUTTON_SYNC);
-                }
+                if (!statusSync()) { pressButton(BUTTON_SYNC); }
 
                 // s-mode off
-                if (bool(ds->xB1[0] & 0x80)) {
-                    pressButton(BUTTON_S_MODE);
-                }
+                if (statusSMode()) { pressButton(BUTTON_S_MODE); }
 
                 // eco off
-                if (bool(ds->xB1[0] & 0x08)) {
-                    pressButton(BUTTON_ECO);
-                }
+                if (statusEco()) { pressButton(BUTTON_ECO); }
 
                 oneTimeSettingsSet = true;
             }
 
             // temp
+            settingsChangedThisRound |= setTemp(ZONE_DRIVER, TEMP_HI);
 
-            int driverTempDelta = getTempDelta(&ds->xB1[4], TEMP_HI);
-            int passengerTempDelta = getTempDelta(&ds->xB1[5], TEMP_HI);
-            if (driverTempDelta != 0) {
-                changeTemp(TEMP_DRIVER, driverTempDelta);
-                settingsChangedThisRound = true;
-            }
-//            if (passengerTempDelta != 0) {
-//                changeTemp(TEMP_PASSENGER, passengerTempDelta);
-//                settingsChangedThisRound = true;
-//            }
-
-            // fan
-            if ((ds->xB1[1] & 7) < 7) {
-                pressButton(BUTTON_FAN_UP);
-                settingsChangedThisRound = true;
-            }
+            // fan speed
+            settingsChangedThisRound |= setFan(7);
 
             // keep changing if not complete
             if (!settingsChangedThisRound) {
                 defrostSettingsSet = true;
             }
-        }
-    }
-
-    static uint8_t calcTemp(uint8_t hexTemp) {
-        if (hexTemp == 0) {
-            return TEMP_LO;
-        } else if (hexTemp >= 0x66) {
-            return hexTemp - 0x2a;
-        } else {
-            return hexTemp + 0x1f;
         }
     }
 
@@ -183,7 +204,7 @@ public:
         if (millis() > 2000) {
             l->log("testIncreaseTemp");
             l->log("before: " + DataStore::frameToString(ds->x39));
-            changeTemp(TEMP_DRIVER, 15);
+            changeTemp(ZONE_DRIVER, 15);
             l->log("after:  " + DataStore::frameToString(ds->x39));
         }
     }
